075d9261cf916d64568138a8a57db301610a9020
Mikhail Sukhorukov <Mikhail_Sukhorukov@epam.com>
2020-02-18 13:48:11 +0300

    a little refactor

--- TestCases/test_yandex_speller.py
+++ TestCases/test_yandex_speller.py
@@ -1,22 +1,9 @@
+from TestData import test_data as data
 import json
 import jsonpath
 import pytest
 import os
 import requests
-
-URL = 'https://speller.yandex.net/services/spellservice.json'
-TEXT = 'checkText'
-TEXTS = 'checkTexts'
-VALID_RU_WORD = 'слово'
-INVALID_RU_WORD = 'словоо'
-VALID_EN_WORD = 'word'
-INVALID_EN_WORD = 'wordd'
-VALID_UK_WORD = 'мова'
-INVALID_UK_WORD = 'моваа'
-BELOW_ZERO = -45
-ZERO = 0
-ABOVE_ZERO = 55
-BIG = 183546
 
 
 # run with: pytest -n3 -m "invalid"
@@ -25,11 +12,12 @@ class TestInvalidWords():
     """ Check invalid words - word must be corrected in response """
 
     @pytest.mark.parametrize("invalid_word, valid_word", (
-            [INVALID_EN_WORD, VALID_EN_WORD], [INVALID_RU_WORD, VALID_RU_WORD], [INVALID_UK_WORD, VALID_UK_WORD]),
+            [data.INVALID_EN_WORD, data.VALID_EN_WORD], [data.INVALID_RU_WORD, data.VALID_RU_WORD],
+            [data.INVALID_UK_WORD, data.VALID_UK_WORD]),
                              ids=["en", "ru", "uk"])
     def test_invalid_word(self, invalid_word, valid_word):
         json_data = {'text': invalid_word}
-        response = requests.get(os.path.join(URL, TEXT), params=json_data)
+        response = requests.get(os.path.join(data.URL, data.TEXT), params=json_data)
         response_json = json.loads(response.text)
         word = jsonpath.jsonpath(response_json[0], 'word')
         s = jsonpath.jsonpath(response_json[0], 's')
@@ -42,11 +30,11 @@ class TestInvalidWords():
 class TestValidWords():
     """ Check valid words - must be empty response """
 
-    @pytest.mark.parametrize("valid_word", (VALID_EN_WORD, VALID_RU_WORD, VALID_UK_WORD),
+    @pytest.mark.parametrize("valid_word", (data.VALID_EN_WORD, data.VALID_RU_WORD, data.VALID_UK_WORD),
                              ids=["en", "ru", "uk"])
     def test_valid_word(self, valid_word):
         json_data = {'text': valid_word}
-        response = requests.get(os.path.join(URL, TEXT), params=json_data)
+        response = requests.get(os.path.join(data.URL, data.TEXT), params=json_data)
         assert response.text == "[]"
 
 
@@ -55,98 +43,100 @@ class TestValidWords():
 class TestDigits():
     """ Check that digits is not acceptable """
 
-    @pytest.mark.parametrize("digit", (BELOW_ZERO, ZERO, ABOVE_ZERO, BIG),
+    @pytest.mark.parametrize("digit", (data.BELOW_ZERO, data.ZERO, data.ABOVE_ZERO, data.BIG),
                              ids=["below zero", "zero", "above zero", "big"])
     def test_valid_word(self, digit):
         json_data = {'text': digit}
-        response = requests.get(os.path.join(URL, TEXT), params=json_data)
+        response = requests.get(os.path.join(data.URL, data.TEXT), params=json_data)
         assert response.text == "[]"
+
 
 # run with: pytest -n9 -m "filtration"
 @pytest.mark.filtration
 class TestLanguages():
     """ By default settings, ru and en languages are turned on """
+
     @pytest.mark.filtration_ru
     class TestLanguageRu():
         """ Check filtration by ru language """
 
         def test_filtration_by_ru_language_ru_word(self):
-            json_data = {'text': INVALID_RU_WORD, 'lang': 'ru'}
-            response = requests.get(os.path.join(URL, TEXT), params=json_data)
+            json_data = {'text': data.INVALID_RU_WORD, 'lang': 'ru'}
+            response = requests.get(os.path.join(data.URL, data.TEXT), params=json_data)
             response_json = json.loads(response.text)
             word = jsonpath.jsonpath(response_json[0], 'word')
             s = jsonpath.jsonpath(response_json[0], 's')
             assert json_data['text'] == word[0]
-            assert VALID_RU_WORD in s[0]
+            assert data.VALID_RU_WORD in s[0]
 
         def test_filtration_by_ru_language_uk_word(self):
-            json_data = {'text': INVALID_UK_WORD, 'lang': 'ru'}
-            response = requests.get(os.path.join(URL, TEXT), params=json_data)
+            json_data = {'text': data.INVALID_UK_WORD, 'lang': 'ru'}
+            response = requests.get(os.path.join(data.URL, data.TEXT), params=json_data)
             assert response.text == "[]"
 
         def test_filtration_by_ru_language_en_word(self):
-            json_data = {'text': INVALID_EN_WORD, 'lang': 'ru'}
-            response = requests.get(os.path.join(URL, TEXT), params=json_data)
+            json_data = {'text': data.INVALID_EN_WORD, 'lang': 'ru'}
+            response = requests.get(os.path.join(data.URL, data.TEXT), params=json_data)
             response_json = json.loads(response.text)
             word = jsonpath.jsonpath(response_json[0], 'word')
             s = jsonpath.jsonpath(response_json[0], 's')
             assert json_data['text'] == word[0]
-            assert VALID_EN_WORD in s[0]
+            assert data.VALID_EN_WORD in s[0]
 
     @pytest.mark.filtration_en
     class TestLanguageEn():
         """ Check filtration by en language """
 
         def test_filtration_by_en_language_ru_word(self):
-            json_data = {'text': INVALID_RU_WORD, 'lang': 'en'}
-            response = requests.get(os.path.join(URL, TEXT), params=json_data)
+            json_data = {'text': data.INVALID_RU_WORD, 'lang': 'en'}
+            response = requests.get(os.path.join(data.URL, data.TEXT), params=json_data)
             response_json = json.loads(response.text)
             word = jsonpath.jsonpath(response_json[0], 'word')
             s = jsonpath.jsonpath(response_json[0], 's')
             assert json_data['text'] == word[0]
-            assert VALID_RU_WORD in s[0]
+            assert data.VALID_RU_WORD in s[0]
 
         def test_filtration_by_en_language_uk_word(self):
-            json_data = {'text': INVALID_UK_WORD, 'lang': 'en'}
-            response = requests.get(os.path.join(URL, TEXT), params=json_data)
+            json_data = {'text': data.INVALID_UK_WORD, 'lang': 'en'}
+            response = requests.get(os.path.join(data.URL, data.TEXT), params=json_data)
             assert response.text == "[]"
 
         def test_filtration_by_en_language_en_word(self):
-            json_data = {'text': INVALID_EN_WORD, 'lang': 'en'}
-            response = requests.get(os.path.join(URL, TEXT), params=json_data)
+            json_data = {'text': data.INVALID_EN_WORD, 'lang': 'en'}
+            response = requests.get(os.path.join(data.URL, data.TEXT), params=json_data)
             response_json = json.loads(response.text)
             word = jsonpath.jsonpath(response_json[0], 'word')
             s = jsonpath.jsonpath(response_json[0], 's')
             assert json_data['text'] == word[0]
-            assert VALID_EN_WORD in s[0]
+            assert data.VALID_EN_WORD in s[0]
 
     @pytest.mark.filtration_uk
     class TestLanguageUk():
         """ Check filtration by uk language """
 
         def test_filtration_by_uk_language_ru_word(self):
-            json_data = {'text': INVALID_RU_WORD, 'lang': 'uk'}
-            response = requests.get(os.path.join(URL, TEXT), params=json_data)
+            json_data = {'text': data.INVALID_RU_WORD, 'lang': 'uk'}
+            response = requests.get(os.path.join(data.URL, data.TEXT), params=json_data)
             response_json = json.loads(response.text)
             word = jsonpath.jsonpath(response_json[0], 'word')
             s = jsonpath.jsonpath(response_json[0], 's')
             assert json_data['text'] == word[0]
-            assert VALID_RU_WORD in s[0]
+            assert data.VALID_RU_WORD in s[0]
 
         def test_filtration_by_uk_language_uk_word(self):
-            json_data = {'text': INVALID_UK_WORD, 'lang': 'uk'}
-            response = requests.get(os.path.join(URL, TEXT), params=json_data)
+            json_data = {'text': data.INVALID_UK_WORD, 'lang': 'uk'}
+            response = requests.get(os.path.join(data.URL, data.TEXT), params=json_data)
             response_json = json.loads(response.text)
             word = jsonpath.jsonpath(response_json[0], 'word')
             s = jsonpath.jsonpath(response_json[0], 's')
             assert json_data['text'] == word[0]
-            assert VALID_UK_WORD in s[0]
+            assert data.VALID_UK_WORD in s[0]
 
         def test_filtration_by_uk_language_en_word(self):
-            json_data = {'text': INVALID_EN_WORD, 'lang': 'uk'}
-            response = requests.get(os.path.join(URL, TEXT), params=json_data)
+            json_data = {'text': data.INVALID_EN_WORD, 'lang': 'uk'}
+            response = requests.get(os.path.join(data.URL, data.TEXT), params=json_data)
             response_json = json.loads(response.text)
             word = jsonpath.jsonpath(response_json[0], 'word')
             s = jsonpath.jsonpath(response_json[0], 's')
             assert json_data['text'] == word[0]
-            assert VALID_EN_WORD in s[0]
+            assert data.VALID_EN_WORD in s[0]
---
+++ TestData/test_data.py
@@ -0,0 +1,15 @@
+""" Test data for yandex speller api tests """
+
+URL = 'https://speller.yandex.net/services/spellservice.json'
+TEXT = 'checkText'
+TEXTS = 'checkTexts'
+VALID_RU_WORD = 'слово'
+INVALID_RU_WORD = 'словоо'
+VALID_EN_WORD = 'word'
+INVALID_EN_WORD = 'wordd'
+VALID_UK_WORD = 'мова'
+INVALID_UK_WORD = 'моваа'
+BELOW_ZERO = -45
+ZERO = 0
+ABOVE_ZERO = 55
+BIG = 183546
