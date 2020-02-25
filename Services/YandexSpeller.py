4e53da7f77c921bf12434010caf1f892478833e2
Mikhail Sukhorukov <Mikhail_Sukhorukov@epam.com>
2020-02-25 15:32:41 +0300

    I have added request data to class with methods and created create_json_for_request method

--- Services/YandexSpeller.py
+++ Services/YandexSpeller.py
@@ -5,13 +5,22 @@ from DataStore.data import Url as url
 
 
 class YandexSpellerApi:
+    REQUEST_TEXT = "text"
+    REQUEST_OPTIONS = "options"
+    REQUEST_LANG = "lang"
+
+    def create_json_for_request(self, content, options="0", lang=""):
+        json_object = {YandexSpellerApi.REQUEST_TEXT: content, YandexSpellerApi.REQUEST_OPTIONS: options,
+                       YandexSpellerApi.REQUEST_LANG: lang}
+        return json_object
+
     def check_text(self, content, options="0", lang=""):
-        json_object = {"text": content, "options": options, "lang": lang}
+        json_object = self.create_json_for_request(content, options, lang)
         response = requests.get(url.CHECK_TEXT, params=json_object)
         return response
 
     def check_texts(self, content, options="0", lang=""):
-        json_object = {"text": content, "options": options, "lang": lang}
+        json_object = self.create_json_for_request(content, options, lang)
         response = requests.get(url.CHECK_TEXTS, params=json_object)
         return response
 
